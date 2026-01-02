69da71dc89e92c0b9a3cd31061d9c5d205024a1e
Junior Matalatala <juniormatalatala29@gmail.com>
2026-01-02 21:56:47 +0100

    yorobox

--- src/pagesSuivant/SuivantTarifs.tsx
+++ src/pagesSuivant/SuivantTarifs.tsx
@@ -1,21 +1,47 @@
-import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
 import { Link } from "react-router-dom";
-import "../styles/Accueil.css";
-import bg from "../assets/image1.jpg"; // ton image
+import { Autoplay, Pagination, Navigation } from "swiper/modules";
+import "../pages/Multimedia.css";
 
-const Accueil: React.FC = () => {
+import "swiper/css/bundle";
+
+const images = [
+  "/images/image1.jpg",
+  "/images/image2.jpg",
+  "/images/image3.jpg",
+  "/images/image4.jpg",
+];
+
+export default function Multimedia() {
   return (
-    <div className="accueil-root">
-      {/* Fond noir complet */}
-      <div className="background-black"></div>
+    <section className="multimedia-section">
+      <div className="glass-card">
+        <h2 className="title">Nos Modèles Signature</h2>
+        <p className="subtitle">
+          Découvrez l’élégance, le style et la perfection de nos coiffures
+        </p>
 
-      {/* Image de fond responsive */}
-      <img src={bg} alt="fond" className="bg-image" />
-
-      {/* Contenu par-dessus */}
+        <Swiper
+          modules={[Autoplay, Pagination, Navigation]}
+          autoplay={{
+            delay: 3500,
+            disableOnInteraction: false,
+          }}
+          pagination={{ clickable: true }}
+          navigation
+          loop
+          className="swiper-container"
+        >
+          {images.map((img, index) => (
+            <SwiperSlide key={index}>
+              <div className="image-wrapper">
+                <img src={img} alt={`Modèle ${index}`} />
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
       <header className="accueil-header">
       </header>
-
       <div className="bottom-section">
         {/* Le bouton mène vers la route /suivanttarifs */}
         <Link to="/formulaire">
@@ -25,8 +51,7 @@ const Accueil: React.FC = () => {
           Une Application Web développée par YoroBox
         </footer>
       </div>
-    </div>
+      </div>
+    </section>
   );
-};
-
-export default Accueil;
+}
